76a13625cad5e09b870d6b67db46bb63d42e9c8a
António Rebelo <tz.ajsr@gmail.com>
2023-01-13 22:00:52 +0000

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/AntonioRebelo/course/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.AntonioRebelo.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.AntonioRebelo.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+
+}
