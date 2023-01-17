6632a13fe001de20639487ff34dc487e685610f4
António Rebelo <tz.ajsr@gmail.com>
2023-01-17 11:39:34 +0000

    Exception handling - update

--- src/main/java/com/AntonioRebelo/course/services/UserService.java
+++ src/main/java/com/AntonioRebelo/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.AntonioRebelo.course.repositories.UserRepository;
 import com.AntonioRebelo.course.services.exceptions.DatabaseException;
 import com.AntonioRebelo.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,9 +47,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	public void updateData(User entity, User obj ) {
